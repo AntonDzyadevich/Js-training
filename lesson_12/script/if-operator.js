68ac6ec5baf49a4f8eac1d9637641c4fdb92b3c9
Anton Dzyadevich <antonbilder86@gmail.com>
2019-12-20 21:35:03 +0300

    add: if-operator

---
+++ lesson_12/script/if-operator.js
@@ -0,0 +1,35 @@
+//let year = prompt('Какой сейчас год');
+
+//if (year == 2019) {
+//  console.log('А ты чертовски прав!');
+//}else{
+//  console.log('А вот не угадал\)');
+//};
+
+//if (year == 2019) {
+//  console.log('Ты прав!');
+//} else if (year > 2019) {
+//  console.log('Ты что из будущего?');
+//}else if(year === null) {
+//  console.log('Ты что шпион?');
+//}else {
+//  console.log('Ты что из прошлого?');
+//}
+
+//let message;
+//let age = prompt('Сколько тебе лет?');
+//message =  (age >= 18) ? 'Заходи!' : 'Пошел вон малой!';
+// 
+//console.log(message);
+
+//let javascript = prompt('Знаешь ли ты официальное название JS?');
+//if (javascript == 'ecmascript') {
+//  console.log('Ты прав!');
+//} else {
+//  console.log('Ты не прав!');
+//}
+
+
+
+
+

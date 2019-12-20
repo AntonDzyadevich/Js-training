0f9dc1d7d03528858e9aaac6be41f6e136f78c3a
Anton Dzyadevich <antonbilder86@gmail.com>
2019-12-20 20:31:39 +0300

    add: Alert Prompt Confirm

---
+++ lesson_12/script/alert-prompt-confirm.js
@@ -0,0 +1,7 @@
+'use strict';
+//let x = alert( 'Alert Prompt Confirm');
+//let x = prompt ('Сколько мне лет?');
+//let x = confirm ('Ты самый умный?');
+let name = prompt('Как твое имя?');
+
+console.log(`Привет, ${name} "!"`);

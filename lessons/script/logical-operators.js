30a433363cca89b2df79b437a1bbae541c17f82b
Anton Dzyadevich <antonbilder86@gmail.com>
2019-12-22 12:36:25 +0300

    add:while-for

---
+++ lessons/script/logical-operators.js
@@ -0,0 +1,15 @@
+//console.log(true || true);
+//console.log(true || false);
+//console.log(false || true);
+//console.log(false || false);
+//
+//console.log(true && true);
+//console.log(true && false);
+//console.log(false && true);
+//console.log(false && false);
+
+//console.log(true != true);
+//console.log(!!0);
+
+let age = 18;
+console.log(age >=18 && age <70);
---
+++ lessons/script/while-for.js
@@ -0,0 +1,80 @@
+'use strict';
+
+//let i = 0;
+//
+//do {
+//  console.log(i);
+//  i++;
+//} while (i < 3);
+
+//for (let i = 0; i<5; i++) {
+//  console.log(i);
+//}
+
+//let i = 0;
+//
+//for (;;) {
+//  console.log(i);
+//  i++;
+//}
+
+//for (let i = 0; i < 5; i++) {
+//  if (i == 3) {
+//    console.log('Я нашел тройку');
+//    break;
+//  } 
+//  console.log('Это не три...');
+//}
+
+//for (let i=0; i<10; i++) {
+//  if (i%2 == 0) {
+//    console.log(`${i} - четное число`);
+//    continue;
+//  }
+//  console.log(`${i} - не четное число`);
+//}
+
+//iter: for (let i=0;i<=5;i++) {
+//  for (let j=0;j<10;j++) {
+//    let  input = confirm(`Ты загадал ${i*10+j}`,'');
+//    if (input) {
+//      alert('Я угадал');
+//      break iter;
+//    }
+//  }
+//}
+//for (let i=2; i<10; i++) {
+//  if (i%2 == 0) {
+//    console.log(i);
+//    continue;
+//  }
+//}
+//  for (let i=2; i<=10; i++) {
+//   if (i%2==0) console.log(i);
+//  }
+
+let result;
+
+do{
+  result=prompt('Задайте число больше 100');
+  console.log(result);
+  if (result!==null){
+    if (result<100 || result ===''){
+      alert ('Ты дурак?');
+  }else{
+    alert('Спасибо');
+    break;
+  }
+  }
+} while(result!==null);
+    
+  
+  
+
+
+
+
+
+
+
+

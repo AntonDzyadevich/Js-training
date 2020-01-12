dd3d206deb82cca30c897907bd536b999d818873
Anton Dzyadevich <antonbilder86@gmail.com>
2020-01-12 13:19:08 +0300

    edit: lesson-14

--- lessons/script/calculator/calculator.js
+++ lessons/script/calculator/calculator.js
@@ -10,8 +10,57 @@
 //  return x * y;
 //}
 
+//let calculator = {
+//  read() {
+//    this.x = +prompt('Задайте число x:', '0');
+//    this.y = +prompt('Задайте число y:', '0');
+//  },
+//  sum() {
+//    return this.x + this.y;
+//  },
+//  mult() {
+//    return this.x * this.y;
+//  }
+//};
+//
+//let ladder= {
+//  step: 0,
+//  up() {
+//    this.step++;
+//    return this;
+//  },
+//  down() {
+//    this.step--;
+//    return this;
+//  },
+//  showStep() {
+//    console.log(this.step);
+//  }
+//}
+//
+
 //END FIRST VERSION
 
+function calculator(){
+  this.read = function() {
+    this.x = +prompt('Задайте число x:', '0');
+    this.y = +prompt('Задайте число y:', '0');
+},
+    this.sum = function() {
+    return this.x + this.y;
+  },
+    this.mult = function() {
+    return this.x * this.y;
+ } 
+}
+
+let calculator = new Calculator();
+
+
+
+
+
+//END second VERSION
 
 //Способы написания обьектов
 //let user = new Object();
@@ -49,12 +98,51 @@
 //  user.name = 'Pete';
 //  delete user.Name;
 
-function isEmpty(obj) {
-  for (let key in obj) {
-    return false;
-  }
-  return true;
-}
+//function isEmpty(obj) {
+//  for (let key in obj) {
+//    return false;
+//  }
+//  return true;
+//}
+ 
+//let user = {
+//  name: 'John',
+//  age: 30 
+//};
+// function sayHi() {
+//  console.log("Привет!");
+//};
+//user.sayHi = sayHi;
+
+//let user = {
+//  name: 'John',
+//  age: 30, 
+//  sayHi() {
+//    console.log('Привет!');
+//  }
+//};
+
+
+//let user = new User('Федя');
+//
+//function User(name) {
+//  //this = {};
+//  this.name = name;
+//  this.isAdmin = false;
+//  //return this;
+////}
+
+//let obj = {};
+//
+//function A() {
+//  return obj;
+//}
+//function B() {
+//  return obj;
+//}
+//
+//let a = new A();
+//let b = new B();
 
 
 
